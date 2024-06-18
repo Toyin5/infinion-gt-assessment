93c0216f002607bdf2658c2e43d7bbabb854a08d
ToyinM <toyinmuhammed50@gmail.com>
2024-06-18 19:43:33 +0100

    Add project files.

---
+++ Core/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string UserId { get; set; }
+    public required string FirstName {get;set;}
+    public required string LastName { get; set; }
+    public string Email { get; set;}
+    public string Password { get; set;}
+    public string UserName { get; set;}
+}
